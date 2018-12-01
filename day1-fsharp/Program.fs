cf4394d09e9630011e49c6dfeb94ef6eaf611329
Oskar Hermansson <oskar.hermansson@gmail.com>
2018-12-01 20:17:33 +0100

    day1*

---
+++ day1-fsharp/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+let convert (str:string) = Convert.ToInt32 str
+let parse lines = Array.map convert lines
+let sum (frequencies:int[]) = Array.sum frequencies
+let read path = File.ReadAllLines path
+
+[<EntryPoint>]
+let main argv =
+    read "input.txt" |> parse |> sum |> Console.WriteLine
+    0
