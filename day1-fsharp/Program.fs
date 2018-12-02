6a942e9d426b0119d91b57dbffda5154b6c9b2ab
Axel Olsson <axel.olsson@gmail.com>
2018-12-02 10:15:25 +0100

    Using fantomas with all "remove space" options disabled

--- day1-fsharp/Program.fs
+++ day1-fsharp/Program.fs
@@ -1,39 +1,63 @@
 ﻿open System
 open System.IO
 
-let convert (str:string) = Convert.ToInt32 str
-let parse lines = Array.map convert lines
-let sum (changes:int[]) = Array.sum changes
+// Helpers
+
+let parse lines = Array.map Int32.Parse lines
+
 let read path = File.ReadAllLines path
-let repeat items = seq { while true do yield! items }
+
+// Take one
+
+let resultingFrequency changes = Array.sum changes
+
+let findFirstDuplicateFrequency changes =
+    let rec findFirstDupFreqHelper (knownFrequencies, currentFrequency)
+            (changes, atChange) =
+        if Set.contains currentFrequency knownFrequencies then currentFrequency
+        else
+            let knownFrequencies = Set.add currentFrequency knownFrequencies
+            let currentFrequency = currentFrequency + Array.get changes atChange
+
+            findFirstDupFreqHelper (knownFrequencies, currentFrequency)
+                (changes, (atChange + 1) % Array.length changes)
+
+    findFirstDupFreqHelper (Set.empty, 0) (changes, 0)
+
+// Take two
+
+let repeat items =
+    seq {
+        while true do
+            yield! items
+    }
+
 let frequencies changes = Seq.scan (+) 0 changes
 
 let firstDuplicate sequence =
-  let rec firstDuplicateHelper uniqueItems currentSeq =
-    if Seq.isEmpty currentSeq then
-      None
-    else
-      let item = Seq.head currentSeq
-      if Set.contains item uniqueItems then
-        Some(item)
-      else
-        let nextPrevious = Set.add item uniqueItems
-        firstDuplicateHelper nextPrevious (Seq.tail currentSeq)
-  firstDuplicateHelper Set.empty sequence
+    let rec firstDuplicateHelper uniqueItems currentSeq =
+        if Seq.isEmpty currentSeq then None
+        else
+            let item = Seq.head currentSeq
+            if Set.contains item uniqueItems then Some(item)
+            else
+                let nextPrevious = Set.add item uniqueItems
+                firstDuplicateHelper nextPrevious (Seq.tail currentSeq)
 
-let rec findDuplicate changes previous currentFrequency changeIndex =
-  if Set.contains currentFrequency previous then
-    currentFrequency
-  else
-    let nextPrevious = Set.add currentFrequency previous
-    let nextFrequency = currentFrequency + Array.get changes changeIndex
-    let nextIndex = (changeIndex + 1) % (Array.length changes)
-    findDuplicate changes nextPrevious nextFrequency nextIndex
+    firstDuplicateHelper Set.empty sequence
 
 [<EntryPoint>]
-let main argv =
+let main _ =
     let changes = read "input.txt" |> parse
-    printfn "Problem 1: %d" (changes |> sum)
-    printfn "Problem 2a: %d" (findDuplicate changes Set.empty 0 0)
-    printfn "Problem 2b: %d" ((firstDuplicate (frequencies (repeat changes))) |> (Option.defaultValue -666))
+
+    printfn "⭐ Problem 1 (take one): %d" (changes |> resultingFrequency)
+    printfn "⭐ Problem 2 (take one): %d" (findFirstDuplicateFrequency changes)
+
+    printfn "⭐ Problem 1 (take two): %d" (changes |> frequencies |> Seq.last)
+    printfn "⭐ Problem 2 (take two): TBA"
+    //     (changes
+    //      |> repeat
+    //      |> frequencies
+    //      |> firstDuplicate
+    //      |> Option.defaultValue -1337)
     0
